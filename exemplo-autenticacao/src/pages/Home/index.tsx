dfed78b435d5b0e0cf663be6989c6b894c8027f2
Sidney Sousa <sidney.sousa@ifms.edu.br>
2022-05-05 18:42:05 -0400

    adicionando autenticação

--- exemplo-autenticacao/src/pages/Home/index.tsx
+++ exemplo-autenticacao/src/pages/Home/index.tsx
@@ -1,5 +1,27 @@
+import { signOut, getAuth } from 'firebase/auth'
+import { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import firebaseApp from '../../config/firebase'
+import { UserContext } from '../../context/UserContext'
+
 const Home = () => {
-  return <h1>Home</h1>
+  const { setToken } = useContext(UserContext)
+  const auth = getAuth(firebaseApp)
+  const navigate = useNavigate()
+
+  const doLogout = () => {
+    signOut(auth)
+    setToken('')
+    navigate('/login')
+  }
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <button onClick={() => doLogout()}>Sair</button>
+    </div>
+  )
 }
 
 export default Home
